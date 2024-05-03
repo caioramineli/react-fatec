f7bc46341e27af53704d5dd49e358f5e13237caf
caioramineli <caiofrancoramineli3@gmail.com>
2024-05-03 10:21:15 -0300

    contador criado

--- src/components/atividade02/Atividade02.jsx
+++ src/components/atividade02/Atividade02.jsx
@@ -1,11 +1,11 @@
-// import Contador from "./Contador";
+import Contador from "./Contador";
 import { Link } from 'react-router-dom';
 
 export default function Atividade01() {
     return (
         <>
             <h1>Atividade 02</h1>
-            {/* <Contador /> */}
+            <Contador />
             <br />
             <Link to="/">Retornar a página Inicial</Link>
         </>
---
+++ src/components/atividade02/Contador.jsx
@@ -0,0 +1,7 @@
+export default function Contador() {
+    return (
+        <>
+            <h1>Contador</h1>
+        </>
+    )
+}
